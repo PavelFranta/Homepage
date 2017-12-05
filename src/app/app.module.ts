abc858ce792629033e9c46ebfa3478a55d35d4c7
Pavel Franta <1.pavel.franta@gmail.com>
2017-12-05 12:14:11 +0000

    rename header to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawe
 
 
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    NavbarComponent,
     FooterComponent
   ],
   imports: [
