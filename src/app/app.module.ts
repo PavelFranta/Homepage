273c2b6ba601eae0dabc3226ddff08f3a190cf76
Pavel Franta <1.pavel.franta@gmail.com>
2017-12-07 01:31:46 +0000

    updating home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ThrilledPavelComponent } from './thrilled-pavel/thrilled-pavel.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { SocialPanelComponent } from './social-panel/social-panel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ThrilledPavelComponent,
+    HomeComponent,
+    SocialPanelComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    Angular2FontawesomeModule
+    Angular2FontawesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/thrilled-pavel/thrilled-pavel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thrilled-pavel',
+  templateUrl: './thrilled-pavel.component.html',
+  styleUrls: ['./thrilled-pavel.component.scss']
+})
+export class ThrilledPavelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
