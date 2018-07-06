ba360316a7d4e5cf6d34efcb815de86f413cf4d5
Pavel Franta <1.pavel.franta@gmail.com>
2018-07-06 12:31:15 +0200

    update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
-
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { ThrilledPavelComponent } from './thrilled-pavel/thrilled-pavel.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { SocialPanelComponent } from './social-panel/social-panel.component';
@@ -28,7 +25,6 @@ import { PassionsCommentsComponent } from './passions-comments/passions-comments
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ThrilledPavelComponent,
     HomeComponent,
     SocialPanelComponent,
     HomeCommentsComponent,
@@ -45,7 +41,6 @@ import { PassionsCommentsComponent } from './passions-comments/passions-comments
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    Angular2FontawesomeModule,
     AppRoutingModule
   ],
   providers: [],
