4cb398f0350cdcde5ec9a967f682dda32d001a35
Pavel Franta <1.pavel.franta@gmail.com>
2019-08-31 19:39:06 +0200

    fixed nav bar for mobilephones
    enhanced responsive design

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  displayBurgerMenu = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    router.events.subscribe(() => {
+      this.displayBurgerMenu = false;
+    });
   }
-
 }
